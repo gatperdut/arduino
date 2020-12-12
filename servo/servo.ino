9cff6696c4b2f0dc96286e7eeefee037b5fa6cc1
carlosr <carlos.romero@onlim.com>
2020-12-12 21:11:14 +0100

    lotsa fuckin round

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,67 +1,33 @@
-#include <Servo.h>
-
-int pin = 9;
-
-Servo servo;
-
-long prevt;
-
-int angles[] = { 0, 45, 90, 45 };
-
-//   0 - 2140
-//  90 - 1480 - 90
-// 180 - 820
-#define XLEFT   2500
-#define LEFT    2140
-#define LCENTER 1810
-#define CENTER  1480
-#define RCENTER 1150
-#define RIGHT    820
-#define XRIGHT   500
-
-int usecs[] = { CENTER, LCENTER, LEFT, XLEFT, LEFT, LCENTER, CENTER, RCENTER, RIGHT, XRIGHT, RIGHT, RCENTER };
-
-//int usecs[] = { CENTER };
-
-int i;
-
+#include <VarSpeedServo.h> 
+ 
+VarSpeedServo myservo;
+ 
 void setup() {
-  servo.attach(pin, 500, 2500);
-
-  Serial.begin(9600);
-
-  i = 0;
-
-  prevt = millis();
-}
-
+  myservo.attach(9);
+  
+  
+  myservo.write(0, 30);
+  
+  myservo.wait();
+  delay(1000);
+Serial.begin(9600);
+} 
+ 
 void loop() {
-//    loop_angles();
-  loop_usecs();
-}
+    myservo.write(0, 30);
+  myservo.wait();
+  delay(1000);
+    myservo.write(90, 30);
+  myservo.wait();
+  delay(1000);
+  myservo.write(180, 30);
+  myservo.wait();
+  delay(1000);
+  myservo.write(90,30);
+  myservo.wait();
+  delay(1000);
+//  myservo.write(180,30);
+//  myservo.wait();
+//  delay(1000);
 
-void loop_angles() {
-  int size = sizeof(angles) / sizeof(angles[0]);
-
-  if (millis() - prevt >= 2000) {
-    prevt = millis();
-
-    servo.write(angles[i]);
-    Serial.println(angles[i]);
-
-    i = (i + 1) % size;
-  }
-}
-
-void loop_usecs() {
-  int size = sizeof(usecs) / sizeof(usecs[0]);
-
-  if (millis() - prevt >= 2000) {
-    prevt = millis();
-
-    servo.writeMicroseconds(usecs[i]);
-    Serial.println(usecs[i]);
-
-    i = (i + 1) % size;
-  }
 }
